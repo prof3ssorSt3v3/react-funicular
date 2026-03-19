462e4bd079e4570e53be47185fc802da81234fa9
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-03-19 11:14:41 -0400

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState, useEffect } from 'react';
+
+function App() {
+  //fetch from TMDB, build a grid of movie cards, img/placeholder, spinner
+
+  return (
+    <>
+      <main>
+        <h2>Bunch of Movies</h2>
+      </main>
+    </>
+  );
+}
+
+export default App;
